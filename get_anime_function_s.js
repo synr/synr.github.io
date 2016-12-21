37b04926e488ea3a184092a488212548ecb0b0bb
synr <oooo00owo00oooo@gmail.com>
2016-12-21 10:17:25 +0800

    tests

--- get_anime_function_s.js
+++ get_anime_function_s.js
@@ -1,16 +1,18 @@
-function get_anime(){
-    var output_string = "		case \"" + document.title.replace(/(.*)\[.*/gi,"$1") + "\":\n			\/\/reg.ReplaceAllString(text, \"$2\")\n			switch reg.ReplaceAllString(text, \"$4\") {\n";
-    var num = 1;
+function get_anime(ad=''){
+    var output_string = "       case \"" + document.title.replace(/(.*)\[.*/gi,"$1") + "\":\n           \/\/reg.ReplaceAllString(text, \"$2\")\n            switch reg.ReplaceAllString(text, \"$4\") {\n";
+    var num = ( ( (ad=='') || (ad==0) )   ?   1  : 0  );
     for (var i = 0; i < document.getElementsByTagName('a').length; i++) {
         if(document.getElementsByTagName('a')[i].href.indexOf('ani.gamer.com.tw\/animeVideo') != -1){
-            output_string += "			case \"" + num + "\":\n" + "				print_string = anime_say + \"" + document.getElementsByTagName('a')[i].href + "\"\n"
-
+            if(num>0){
+                        output_string += "          case \"" + num + "\":\n" + "                print_string = anime_say + \"" + document.getElementsByTagName('a')[i].href + "\"\n";
+            }
             num++;
         }
     }
-    output_string += "			default:\n			}";
+    output_string += "          default:\n          }";
     console.log(output_string)
     //return output_string;
 }
 
-get_anime();
+get_anime(0);//get_anime(); //沒廣告的時候
+get_anime(1); //有廣告的時候
