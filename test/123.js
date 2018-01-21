d58e362e8d8bbe06b31e168d2cb29c7daa070096
synr <synr@users.noreply.github.com>
2018-01-22 01:57:07 +0800

    Update 123.js

--- test/123.js
+++ test/123.js
@@ -1,4 +1,4 @@
-﻿var print_string = `<html><head>
+var print_string = `<html><head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width">
 <title></title>
@@ -18,7 +18,7 @@
                 for (i = 0; i < get_data_array.length; i++) { 
                     sent_array[i] = get_data_array[i].split(/\\t|，|\\s{4}|\;/g);
                     if((sent_array[i]!==undefined) && (i!=0) ) {
-                        if((sent_array[i][0]!==undefined) && (sent_array[i][1]!==undefined) && (sent_array[i][2]!==undefined) ){
+                        if((sent_array[i][0]!==undefined) && (sent_array[i][1]!==undefined) && (sent_array[i][2]!==undefined) && (sent_array[i][0]!='undefined') && (sent_array[i][1]!='undefined') && (sent_array[i][2]!='undefined') ){
                             console.log(i + ' = YYY');
                             if(i==1){
                                 table_1 = '<div style="page-break-after: always;page-break-inside: avoid;"><table border="0" cellpadding="0" cellspacing="0" style="width: 600px;"><tbody><tr style="height: 70px;"><td>                    &nbsp;</td>                <td>                    <p>                        <font size="4"><b></b></font></p>                    <p>                        <font size="4"><font size="3"></font></font></p>                    <p>                        <br>                                                &nbsp;</p>                </td>                <td>                    &nbsp;</td>                <td>                    &nbsp;</td>            </tr>            <tr>                <td>                    &nbsp;</td>                <td>                    &nbsp;</td>                <td>                    &nbsp;</td>                <td>                                        <p>                        <font size="5">' + sent_array[1][1] + '</font></p>                    <p>                        <font size="5"><font size="5">' + sent_array[1][2] + '</font></font></p>                    <center><p>                        <font size="5"><span style="font-size:40px;">' + sent_array[1][0] + '</span> 先生 / 小姐 收</font></p></center>                </td>            </tr>            <tr>                <td>                    &nbsp;</td>                <td>                    &nbsp;</td>                <td>                    &nbsp;</td>                <td align="right">                    　<br>&nbsp;</td>            </tr>        </tbody>    </table></div>';
@@ -54,11 +54,11 @@
 <body onload="document.getElementsByTagName('textarea')[0].focus();">
 <textarea id="textarea" style="height: 200px;width: 500px" class="noprint">
 收件人    郵遞區號    地址
-中華基    10093    台北市中正區寧波東街
+中華    10093    台北市中正區寧波東街
 </textarea>
 <div class="noprint">文字列的分隔可用，; TAB 跟四個半形空白</div>
 <!--<button class="noprint" onclick="javascript:document.getElementById('print_zone').innerHTML = table_1;">產生信封</button>-->
-<button class="noprint" onclick="javascript:print()">列印</button>
+<button class="noprint" onclick="javascript:print()">列印</button> <button class="noprint" onclick="javascript:Window.location.reload()">回收據</button>
 <center>
     <div style="break-after: page; break-inside: avoid; font-family: &quot;Gen Jyuu Gothic Medium&quot; ,genjyuugothicx_p_medium, 微軟正黑體, &quot;Microsoft JhengHei&quot;, sans-serif, 微軟正黑體, &quot;Microsoft JhengHei&quot;, sans-serif;">
         <center id="print_zone">
